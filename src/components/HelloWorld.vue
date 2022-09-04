1b6c961faf2628b82c0a5e0440b47e2fd93850e2
fahadchowdhary007 <fahadchowdhary007@gmail.com>
2022-09-04 15:42:02 +0530

    changes pushed of frontend

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+<h3>{{textApi}}</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      textApi : '',
+    }
+  },
+  beforeMount(){
+    this.firstApi()
+  },
+  methods:{
+    firstApi(){
+      console.log('First Api before')
+      fetch("http://localhost:8081/firstBackend").then( (response)=> {
+          return response.json()
+      }).then((response)=>
+      {
+        this.textApi = response.text;
+        console.log('First api', response);
+      })
+    }
+  }
+}
+</script>
+<style>
+
+</style>
+
